656980bf946f2fe6c3c9245ba448e18f20300d5e
mmarturini <m.marturini22@gmail.com>
2023-04-26 07:31:34 +0200

    Add project files.

---
+++ TinyRenderer.cpp
@@ -0,0 +1,12 @@
+﻿// TinyRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "TinyRenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

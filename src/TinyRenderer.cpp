a2f85f08e9a7f306f9de070061224aa6d81a5448
mmarturini <m.marturini22@gmail.com>
2023-06-01 10:25:53 +0200

    Add line contour traingles

--- src/TinyRenderer.cpp
+++ src/TinyRenderer.cpp
@@ -11,12 +11,18 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255); // last value is for transparency (RGBa)
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
 const int width = 800;
 const int height = 800;
 
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color) {
+void line(Vec2i v0, Vec2i v1, TGAImage& image, const TGAColor& color) {
+	int x0{ v0.u };
+	int x1{ v1.u };
+	int y0{ v0.v };
+	int y1{ v1.v };
+
 	bool steep = false;
 	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
 		std::swap(x0, y0);
@@ -48,6 +54,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
 	}
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+	line(t0, t1, image, color);
+	line(t0, t2, image, color);
+	line(t1, t2, image, color);
+}
+
 
 int main(int argc, char** argv) {
 
@@ -64,6 +76,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	/* African face rendering
 	TGAImage image(width, height, TGAImage::RGB);
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
@@ -77,6 +90,15 @@ int main(int argc, char** argv) {
 			line(x0, y0, x1, y1, image, white);
 		}
 	}
+	*/
+	TGAImage image(width, height, TGAImage::RGB);
+	Vec2i t0[3] = { Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80) };
+	Vec2i t1[3] = { Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180) };
+	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+	triangle(t0[0], t0[1], t0[2], image, red);
+	triangle(t1[0], t1[1], t1[2], image, white);
+	triangle(t2[0], t2[1], t2[2], image, green);
 
 	image.flip_vertically(); // I want to have the origin at left bottom corner of image
 	image.write_tga_file("output.tga");
